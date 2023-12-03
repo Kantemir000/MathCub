e5cedbbd2ae81ed8fd09b19903cce831a19703f8
tugarinzmey <m.vnukov@yandex.ru>
2023-12-03 15:42:33 +0300

    little fixes

--- MathCub/src/Home/formulas/FisherSnedecor/generatePopulation.js
+++ MathCub/src/Home/formulas/FisherSnedecor/generatePopulation.js
@@ -3,7 +3,7 @@ import jStat from "jstat"
 const generatePopulation = (mean, sd, size) => {
     const population = [];
     for (let i = 0; i < size; i++) {
-        population.push(jStat.normal.sample(mean, sd));
+        population.push(jStat.normal.sample(sd, mean));
     }
     return population;
 };
--- MathCub/src/Home/formulas/chiSquare.js
+++ MathCub/src/Home/formulas/chiSquare.js
@@ -1,13 +1,15 @@
 import * as ss from "simple-statistics"
-import uniqueInputs from "./uniqueInputs";
 
 function calculateChiSquare(intervals, inputs) {
-    const unique = uniqueInputs(inputs) || 1
-    const chiSquare = intervals.reduce(
-        (acc, interval) => 
-        acc += Math.pow(interval.percent - interval.probability * unique, 2) / interval.probability * unique, 0)
+    let n = inputs.length > 1 ? inputs.length : 2;
+    const a = 0.05;
 
-    const tableValue = ss.chiSquaredDistributionTable[unique][0.05]
+    const chiSquare = intervals.reduce(
+        (acc, interval) =>
+            acc += Math.pow(interval.percent - interval.probability * n, 2) / interval.probability * n, 0)
+
+
+    const tableValue = ss.chiSquaredDistributionTable[n - 1][a]
     return {
         chiSquare,
         chiSquareTableValue: tableValue,
