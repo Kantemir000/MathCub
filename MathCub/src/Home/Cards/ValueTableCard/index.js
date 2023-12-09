488a3a37501027437803c82c27f37e793cf95f8e
tugarinzmey <m.vnukov@yandex.ru>
2023-12-09 15:33:24 +0300

    tableInputForm fixed and improved.
    Now it is completly reusable

--- MathCub/src/Home/Cards/ValueTableCard/index.js
+++ MathCub/src/Home/Cards/ValueTableCard/index.js
@@ -1,12 +1,29 @@
 import TableInputForm from "./TableInputForm";
 import Card from "../../../common/components/Card";
 import StyledCardDescription from "../../../common/components/Card/StyledCardContent/StyledCardDescription";
+import { useDispatch, useSelector } from "react-redux";
+import { changeInput, addInput } from "../../../store/slices/randomInputSlice";
 
 const ValueTableCard = () => {
+    const dispatch = useDispatch();
+
+    const { inputs: data } = useSelector((state) => state.randomInputSlice);
+
+    const handleAdd = () => {
+        dispatch(addInput());
+    }
+
+    const handleChange = (targetValue, id, isLast) => {
+        if (isLast) {
+            dispatch(addInput())
+        }
+        dispatch(changeInput({ id, value: targetValue }))
+    };
+
     return (
         <Card title="Случайная величина">
             <StyledCardDescription>Задаем значения случайной величины:</StyledCardDescription>
-            <TableInputForm />
+            <TableInputForm handleAdd={handleAdd} data={data} handleChange={handleChange} />
         </Card>
     )
 }
